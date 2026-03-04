1e81b501173fdda0fb2cb6d27e054bcdb7c52e1a
Christian Krause <122640925+avreo1a@users.noreply.github.com>
2026-03-04 17:43:20 -0500

    Quartz sync: Mar 4, 2026, 5:43 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -2,19 +2,22 @@ import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } fro
 import style from "./styles/footer.scss"
 import { version } from "../../package.json"
 import { i18n } from "../i18n"
+import { pathToRoot, joinSegments } from "../util/path"
 
 interface Options {
   links: Record<string, string>
 }
 
 export default ((opts?: Options) => {
-  const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
+  const Footer: QuartzComponent = ({ displayClass, cfg, fileData }: QuartzComponentProps) => {
     const year = new Date().getFullYear()
     const links = opts?.links ?? []
+    const baseDir = pathToRoot(fileData.slug!)
+    const goatPath = joinSegments(baseDir, "static/goat.png")
     return (
       <footer class={`${displayClass ?? ""}`}>
         <div class="footer-goat">
-          <img src="/static/goat.png" alt="goat" class="spinning-goat" />
+          <img src={goatPath} alt="goat" class="spinning-goat" />
         </div>
         <ul>
           {Object.entries(links).map(([text, link]) => (
